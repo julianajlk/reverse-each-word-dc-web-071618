74f70ad4ba071f2d1336f003d5d0dadbae20f739
Juliana Kang <julianajlk@gmail.com>
2018-07-06 21:52:19 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++
@@ -1,8 +0,0 @@
-def reverse_each_word(string)
-  words = string.split("")
-  reversed_words = words.collect do |word|
-    word.reverse
-  end
-  reversed_string = reversed_words.join(" ")
-  return reversed_string
-end
