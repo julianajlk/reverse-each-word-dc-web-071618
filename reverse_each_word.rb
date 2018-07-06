ecf78c30b50ed591827509d400ed35be0a98bb07
Juliana Kang <julianajlk@gmail.com>
2018-07-06 21:46:22 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,7 +5,7 @@ def reverse_each_word(string)
   words.each do |word|
     end_loop = word.length
     new_string = ''
-  while end_loop >0
+  while end_loop > 0
     end_loop -= 1
     new_string += word[end_loop]
   end
